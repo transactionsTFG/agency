25d9970e49c15aebb8d1fcbe88344ad9b1899946
danielfdez17 <daniel17dev@gmail.com>
2024-10-28 16:18:05 +0100

    finally new rooms are added

--- src/main/webapp/public/js/vuelo-y-hotel.js
+++ src/main/webapp/public/js/vuelo-y-hotel.js
@@ -5,31 +5,33 @@ var fechaSalida = document.getElementById("fecha-salida");
 var botonAnyadirHabitacion = document.getElementById("anyadir-habitacion");
 var botonEliminarHabitacion = document.getElementById("eliminar-habitacion");
 var listaDropdownHabitaciones = document.getElementById("dropdown-habitaciones");
+var botonInfoPasajerosHabitaciones = document.getElementById("btn-info-pasajeros-habitaciones");
+var contadorPasajeros = 1;
+var contadorHabitaciones = 1;
 
 
 function anyadirHabitacion() {
-	/*alert(`Hay ${listaDropdownHabitaciones.children.length} habitaciones`);*/
-	if (listaDropdownHabitaciones.children.length < 7) {
-		listaDropdownHabitaciones.appendChild(
-			"<li>" + 
-				"<div class='card' style='width: 18rem;'>" +
-				  "<div class='card-body'>" +
-				    "<h5 class='card-title'>Habitación Z</h5>" +
-				    "<div id='info-ocupantes'>" +
-			    		"<div>Adultos</div>" +
-				        	"<div>" +
-					    		"<button type='button'>-</button>" +
-					    		"<span>1</span>" +
-					    		"<button type='button'>+</button>" +
-				    	"</div>" +
-				    "</div>" +
-    			  "</div>" +
-				"</div>" +
-		    "</li>");
-	} else {
-		
-	}
+	botonInfoPasajerosHabitaciones.value = `${contadorPasajeros} pasajeros, ${contadorHabitaciones} habitaciones, Cualquier clase`;
+	var li = document.createElement("li");
+	li.innerHTML = `
+			<div class='card' style='width: 18rem;'> 
+			  <div class='card-body'> 
+			    <h5 class='card-title'>Habitación ${contadorHabitaciones}</h5> 
+			    <div id='info-ocupantes'> 
+		    		<div>Adultos</div> 
+		        	<div> 
+			    		<button type='button'>-</button> 
+			    		<span>1</span> 
+			    		<button type='button'>+</button> 
+			    	</div> 
+			    </div> 
+			  </div> 
+			</div>`;
+		listaDropdownHabitaciones.appendChild(li);
 }
 
 function eliminarHabitacion() {
 }
+
+botonAnyadirHabitacion.addEventListener("click", anyadirHabitacion);
+botonEliminarHabitacion.addEventListener("click", eliminarHabitacion);
