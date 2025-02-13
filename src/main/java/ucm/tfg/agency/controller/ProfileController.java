448a4ad6c79262637ad825c9470c5c463bce241d
danielfdez17 <daniel17dev@gmail.com>
2025-02-13 08:15:47 +0100

    FIX: restored profile view

---
+++ src/main/java/ucm/tfg/agency/controller/ProfileController.java
@@ -0,0 +1,15 @@
+package ucm.tfg.agency.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ProfileController {
+    
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+    
+}
