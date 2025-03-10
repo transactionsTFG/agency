1f94006a3fa8ac8e3586522af81ab023e38f2f52
danielfdez17 <daniel17dev@gmail.com>
2025-03-10 20:08:30 +0100

    FEAT: called hotel and airline MTA methods from businessDelegate

--- src/main/java/ucm/tfg/agency/business/businessdelegate/BusinessDelegate.java
+++ src/main/java/ucm/tfg/agency/business/businessdelegate/BusinessDelegate.java
@@ -2,12 +2,23 @@ package ucm.tfg.agency.business.businessdelegate;
 
 import java.util.List;
 
-import ucm.tfg.agency.common.dto.hotel.room.RoomDTO;
 import ucm.tfg.agency.common.dto.patternresult.Result;
 import ucm.tfg.agency.common.dto.user.LoginUserDTO;
 import ucm.tfg.agency.common.dto.user.RegisterUserDTO;
 import ucm.tfg.agency.common.dto.user.ReponseUserDTO;
 import ucm.tfg.agency.common.enums.TypeService;
+import ucm.tfg.agency.soapclient.airlineflight.FlightListDTO;
+import ucm.tfg.agency.soapclient.airlineflight.FlightSOAP;
+import ucm.tfg.agency.soapclient.airlineflight.ParamSearchFlightSOAP;
+import ucm.tfg.agency.soapclient.airlinereservation.AgencyReservationSuccessDTO;
+import ucm.tfg.agency.soapclient.airlinereservation.MakeFlightReservationSOAP;
+import ucm.tfg.agency.soapclient.airlinereservation.ModifyFlightReservationRequestionSOAP;
+import ucm.tfg.agency.soapclient.airlinereservation.UpdateReservationDTO;
+import ucm.tfg.agency.soapclient.hotelbooking.BookingDTO;
+import ucm.tfg.agency.soapclient.hotelbooking.MakeBookingReservationDTO;
+import ucm.tfg.agency.soapclient.hotelbooking.ModifyBookingReservationDTO;
+import ucm.tfg.agency.soapclient.hotelroom.RoomDTO;
+import ucm.tfg.agency.soapclient.hotelroom.RoomListDTO;
 
 public class BusinessDelegate {
     private final BusinessLookup lookupService = new BusinessLookup();
@@ -19,8 +30,52 @@ public class BusinessDelegate {
 
     //TODO... Agregar mas metodos de Agencia
 
-    public void getFlight(long idFlight) {
-        this.lookupService.getAirlineService(this.typeService).getFlight(idFlight);
+    public FlightSOAP getFlight(long idFlight) {
+        return this.lookupService.getAirlineService(this.typeService).getFlightById(idFlight);
+    }
+
+    public List<FlightListDTO> getAllFlights(ParamSearchFlightSOAP params) {
+        return this.lookupService.getAirlineService(typeService).getAllFlights(params);
+    }
+
+    public AgencyReservationSuccessDTO makeFlightReservation(MakeFlightReservationSOAP flightReservationDTO) {
+        return this.lookupService.getAirlineService(typeService).makeFlightReservation(flightReservationDTO);
+    }
+
+    public UpdateReservationDTO modifyFlightReservation(ModifyFlightReservationRequestionSOAP flightReservationDTO) {
+        return this.lookupService.getAirlineService(typeService).modifyFlightReservation(flightReservationDTO);
+    }
+
+    public double cancelFlightReservation(long flightReservationId) {
+        return this.lookupService.getAirlineService(typeService).cancelFlightReservation(flightReservationId);
+    }
+
+    public RoomDTO getRoomById(long roomId) {
+        return this.lookupService.getHotelService(typeService).getRoomById(roomId);
+    }
+
+    public List<RoomListDTO> getAllRooms(String hotelName, String countryName) {
+        return this.lookupService.getHotelService(typeService).getAllRooms(hotelName, countryName);
+    }
+
+    public BookingDTO makeHotelBooking(MakeBookingReservationDTO booking, long userId, String dni) {
+        return this.lookupService.getHotelService(typeService).makeHotelBooking(booking, userId, dni);
+    }
+
+    public BookingDTO modifyHotelBooking(ModifyBookingReservationDTO booking) {
+        return this.lookupService.getHotelService(typeService).modifyHotelBooking(booking);
+    }
+
+    public double cancelHotelBooking(long bookingId) {
+        return this.lookupService.getHotelService(typeService).cancelHotelBooking(bookingId);
+    }
+
+    public double cancelHotelBookingLine(long bookingId, long roomId) {
+        return this.lookupService.getHotelService(typeService).cancelHotelBookingLine(bookingId, roomId);
+    }
+
+    public BookingDTO getHotelBooking(long bookingId) {
+        return this.lookupService.getHotelService(typeService).getHotelBooking(bookingId);
     }
 
     public Result<Long> createUser(final RegisterUserDTO registerUserDTO) {
@@ -29,10 +84,6 @@ public class BusinessDelegate {
 
     public Result<ReponseUserDTO> loginUser(final LoginUserDTO userDTO) {
         return this.lookupService.getUserService(this.typeService).loginUser(userDTO);
-    }
-
-    public List<RoomDTO> getRooms() {
-        return this.lookupService.getHotelService(this.typeService).getRooms();
     }
 
     
--- src/main/java/ucm/tfg/agency/business/businessdelegate/BusinessLookup.java
+++ src/main/java/ucm/tfg/agency/business/businessdelegate/BusinessLookup.java
@@ -1,36 +1,56 @@
 package ucm.tfg.agency.business.businessdelegate;
 
+import ucm.tfg.agency.business.businessdelegate.agency.AgencyMSAService;
+import ucm.tfg.agency.business.businessdelegate.agency.AgencyMTAService;
+import ucm.tfg.agency.business.businessdelegate.agency.AgencyService;
+import ucm.tfg.agency.business.businessdelegate.airline.AirlineMSAService;
 import ucm.tfg.agency.business.businessdelegate.airline.AirlineMTAService;
 import ucm.tfg.agency.business.businessdelegate.airline.AirlineService;
+import ucm.tfg.agency.business.businessdelegate.hotel.HotelMSAService;
 import ucm.tfg.agency.business.businessdelegate.hotel.HotelMTAService;
 import ucm.tfg.agency.business.businessdelegate.hotel.HotelService;
+import ucm.tfg.agency.business.businessdelegate.user.UserMSAService;
 import ucm.tfg.agency.business.businessdelegate.user.UserMTAService;
 import ucm.tfg.agency.business.businessdelegate.user.UserService;
 import ucm.tfg.agency.common.enums.TypeService;
 
 public class BusinessLookup {
+
+    private final AgencyMTAService agencyMTAService;
+    private final AgencyMSAService agencyMSAService;
     private final AirlineMTAService airlineMTAService;
+    private final AirlineMSAService airlineMSAService;
     private final HotelMTAService hotelMTAService;
+    private final HotelMSAService hotelMSAService;
     private final UserMTAService userMTAService;
+    private final UserMSAService userMSAService;
+
 
     public BusinessLookup() {
+        this.agencyMTAService = new AgencyMTAService();
+        this.agencyMSAService = null;
         this.airlineMTAService = new AirlineMTAService();
+        this.airlineMSAService = null;
         this.hotelMTAService = new HotelMTAService();
+        this.hotelMSAService = null;
         this.userMTAService = new UserMTAService();
+        this.userMSAService = null;
     }
 
-    public AirlineService getAirlineService(TypeService typeService){
-        if (typeService == TypeService.MTA) return this.airlineMTAService; 
-            else  return null; //TODO: Aqui habria que agregar el MSA
+    public AgencyService getAgencyMTAService(TypeService typeService) {
+        return typeService.name().equals(TypeService.MTA.name()) ? this.agencyMTAService : this.agencyMSAService;
+    }
+    
+    public AirlineService getAirlineService(TypeService typeService) {
+        return typeService.name().equals(TypeService.MTA.name()) ? this.airlineMTAService : this.airlineMSAService;
     }
 
     public HotelService getHotelService(TypeService typeService) {
-        return typeService.name().equals(TypeService.MTA.name()) ? this.hotelMTAService : null;
+        return typeService.name().equals(TypeService.MTA.name()) ? this.hotelMTAService : this.hotelMSAService;
     }
-
-    public UserService getUserService(TypeService typeService){
-        if (typeService == TypeService.MTA) return this.userMTAService; 
-            else  return null; //TODO: Aqui habria que agregar el MSA
+    
+    public UserService getUserService(TypeService typeService) {
+        return typeService.name().equals(TypeService.MTA.name()) ? this.userMTAService : this.userMSAService;
     }
 
 }
