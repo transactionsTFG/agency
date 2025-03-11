f2bcb48d40c76adce23c0f47c6e5c323397ca28c
josevillacres46 <jvillacr@ucm.es>
2025-03-11 01:45:59 +0100

    FEAT: Add index view with data ofs MTAS

--- src/main/java/ucm/tfg/agency/controller/RootController.java
+++ src/main/java/ucm/tfg/agency/controller/RootController.java
@@ -1,18 +1,42 @@
 package ucm.tfg.agency.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+import ucm.tfg.agency.business.services.agency.AgencyService;
+import ucm.tfg.agency.business.services.airline.AirlineService;
+import ucm.tfg.agency.business.services.hotel.HotelService;
+import ucm.tfg.agency.common.dto.agency.ListFlightHotelDTO;
+import ucm.tfg.agency.common.dto.airline.FlightAirlineInfoDTO;
+import ucm.tfg.agency.common.dto.hotel.RoomInfoDTO;
+import ucm.tfg.agency.common.dto.patternresult.Result;
+
 @Controller
+@AllArgsConstructor
 public class RootController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    private final AgencyService agencyService;
+    private final AirlineService airlineService;
+    private final HotelService hotelService;
 
-    @GetMapping("/index")
-    public String indexTemplate() {
+    @GetMapping({ "/", "index" })
+    public String index(Model model) {
+        Result<Map<String, ListFlightHotelDTO>> resultFlightsAndHotels = agencyService.getFlightsAndHotels(null, null, null, null, null, null);
+        Result<List<FlightAirlineInfoDTO>> resultFlights = airlineService.getAllFlights(null, null, null, null, null);
+        Result<List<RoomInfoDTO>> getAllRooms = hotelService.getAllRooms(null, null);
+        Map<String, ListFlightHotelDTO> data = resultFlightsAndHotels.isSuccess() ? resultFlightsAndHotels.getData() : new HashMap<>();
+        List<FlightAirlineInfoDTO> flights = resultFlights.isSuccess() ? resultFlights.getData().stream().limit(3).toList() : new ArrayList<>();
+        List<RoomInfoDTO> rooms = getAllRooms.isSuccess() ? getAllRooms.getData().stream().limit(4).toList() : new ArrayList<>();   
+        model.addAttribute("flightsAndHotels", data);
+        model.addAttribute("flights", flights);
+        model.addAttribute("rooms", rooms);
         return "index";
     }
 
