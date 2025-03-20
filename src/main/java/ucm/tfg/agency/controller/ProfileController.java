a13457bd7dab6f213e1802f915090f3d483ba00f
josevillacres46 <jvillacr@ucm.es>
2025-03-20 17:49:56 +0100

    FEAT: Add Cancel Reservation

--- src/main/java/ucm/tfg/agency/controller/ProfileController.java
+++ src/main/java/ucm/tfg/agency/controller/ProfileController.java
@@ -7,11 +7,17 @@ import org.springframework.context.MessageSource;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
 import ucm.tfg.agency.business.services.agency.AgencyService;
+import ucm.tfg.agency.business.services.airline.AirlineService;
+import ucm.tfg.agency.business.services.hotel.HotelService;
 import ucm.tfg.agency.common.auth.AuthUser;
 import ucm.tfg.agency.common.dto.agency.TravelDTO;
 import ucm.tfg.agency.common.dto.patternresult.Result;
@@ -22,6 +28,8 @@ import ucm.tfg.agency.common.dto.patternresult.Result;
 public class ProfileController {
     
     private final AgencyService agencyService;
+    private final AirlineService airlineService;
+    private final HotelService hotelService;
     private final MessageSource messageSource;
 
     @GetMapping
@@ -38,6 +46,22 @@ public class ProfileController {
 
         model.addAttribute("totalReservas", totalReservas);
         return "profile";
+    }
+
+    @PostMapping("/removeTravel/{idFlight}/{idHotel}")
+    public String deleteTravel(@PathVariable long idFlight, @PathVariable long idHotel, RedirectAttributes redirectAttributes) {
+        Result<Double> result = null;
+        if(idFlight > 0 && idHotel > 0) 
+            result = this.agencyService.cancelFlightAndHotelReservation(idFlight, idHotel);
+        
+        if(idFlight > 0 && idHotel <= 0) 
+            result = this.airlineService.cancelFlightReservation(idFlight);
+
+        if(idFlight <= 0 && idHotel > 0) 
+            result = this.hotelService.cancelHotelBooking(idHotel);
+    
+        redirectAttributes.addFlashAttribute("priceReturn", result.getData());
+        return "redirect:/profile";
     }
     
 }
