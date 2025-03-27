27b5b981544a76e8829ba30f9cd150ccc3ca3624
josevillacres46 <jvillacr@ucm.es>
2025-03-27 16:52:33 +0100

    FEAT: Add View Checkout

--- src/main/java/ucm/tfg/agency/controller/CheckoutController.java
+++ src/main/java/ucm/tfg/agency/controller/CheckoutController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.AllArgsConstructor;
 import ucm.tfg.agency.business.services.airline.AirlineService;
 import ucm.tfg.agency.business.services.hotel.HotelService;
-import ucm.tfg.agency.common.dto.airline.FlightAirlineDTO;
 import ucm.tfg.agency.common.dto.hotel.RoomDTO;
 import ucm.tfg.agency.common.dto.patternresult.Result;
 import ucm.tfg.agency.soapclient.airlineflight.FlightInstanceDTO;
@@ -24,16 +23,18 @@ public class CheckoutController {
 
     @GetMapping("/{idHotel}/{idFlightInstance}")
     public String checkout(@PathVariable long idHotel, @PathVariable long idFlightInstance, Model model) {
-
+        int price = 0;
         if (idHotel > 0) {
-            Result<RoomDTO> room = this.hotelService.getRoomById(idHotel);
-            model.addAttribute("room", room.getData());
+            RoomDTO room = this.hotelService.getRoomById(idHotel).getData();
+            price += room.getDailyPrice();
+            model.addAttribute("room", room);
         }
         if (idFlightInstance > 0) {
             FlightInstanceDTO flight = this.airlineService.searchFlightInstance(idFlightInstance);
+            price += flight.getPrice();
             model.addAttribute("flight", flight);
         }
-
+        model.addAttribute("price", price);
         return "checkout";
     }
 
